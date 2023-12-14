f4a9436cf8317458508293ce0a55314a0ba4d3cb
Josiah Korzan <josiahkorzan4@gmail.com>
2023-12-13 22:32:42 -0700

    Completed part 1 day 1

--- Day 1/day1.c
+++ Day 1/day1.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
 
     FILE* input;
-    char myString[100];
+
     input = fopen("input.txt", "r");
-    
-    // If the file exist
-    if(input != NULL) {
-        // Read the content and print it
-        while(fgets(myString, 100, input)) {
-            printf("%s", myString);
+    int sum = 0;
+    char* current_line = (char*) malloc(100*sizeof(char));
+    while (fscanf(input, "%s", current_line) != EOF) {
+        char* number = calloc(16, sizeof(char));
+        char first_num;
+        char last_num;
+        int counter = 0;
+        for(char* ptr = current_line; *ptr != '\0'; ptr++) {
+            if (*ptr >= '0' && *ptr <= '9') {
+                if (counter == 0) {
+                    first_num = *ptr;
+                    last_num = *ptr;
+                } else {
+                    last_num = *ptr;
+                }
+                counter++;
+            }
         }
-    } else {
-        printf("Not able to open the file.\n");
+        strncat(number, &first_num, 1);
+        strncat(number, &last_num, 1);
+        sum += atoi(number);
+        printf("%d\n", atoi(number));
     }
-    
+    printf("%d", sum);
+
     fclose(input);
-
-
+    free(current_line);
 
     return 0;
 }
